af791de40f99f387fa415e6acab03e0145ed1fbe
deonneon <dtnifti@gmail.com>
2023-11-24 16:48:41 -0600

    fixed food input

--- src/FoodInput.js
+++ src/FoodInput.js
@@ -29,7 +29,7 @@ const FoodInput = () => {
     const [hideAIResponse, setHideAIResponse] = useState(false);
 
     const handleInputChange = (e) => {
-        const lowercasedValue = e.target.value ? e.target.value.toLowerCase() : 'bad return';;
+        const lowercasedValue = e.target.value ? e.target.value.toLowerCase() : '';;
         setInput(lowercasedValue);
         if (lowercasedValue) {
             const matches = Object.keys(database).filter(food =>
