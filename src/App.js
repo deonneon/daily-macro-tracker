66d3b85f6b41ea15dba183883b8af943e00e8e09
deonneon <dtnifti@gmail.com>
2025-04-08 22:45:14 -0500

    update layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
-import './styles.css';
-import { DietProvider } from './DietContext';
-import Navbar from './Navbar';
-import FoodInput from './FoodInput';
-import DailyDietList from './DailyDietList';
-import Dashboard from './Dashboard';
-import DatabasePage from './DatabasePage';
-import IconButton from './IconButton';
-import QAPage from './QAPage';
-import riceIcon from './icons/rice.png';
-import bananaIcon from './icons/banana.png';
-import { FaBars } from 'react-icons/fa'; // FaBars is a common hamburger icon in Font Awesome
-
+import React, { useState } from "react";
+import "./styles.css";
+import { DietProvider } from "./DietContext";
+import Navbar from "./Navbar";
+import FoodInput from "./FoodInput";
+import DailyDietList from "./DailyDietList";
+import Dashboard from "./Dashboard";
+import DatabasePage from "./DatabasePage";
+import IconButton from "./IconButton";
+import QAPage from "./QAPage";
+import riceIcon from "./icons/rice.png";
+import bananaIcon from "./icons/banana.png";
+import { FaBars } from "react-icons/fa"; // FaBars is a common hamburger icon in Font Awesome
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('main');
+  const [currentPage, setCurrentPage] = useState("main");
 
   return (
     <DietProvider>
       <div className="App">
-        <div style={{ display: 'none' }}>
+        <div style={{ display: "none" }}>
           <Navbar setCurrentPage={setCurrentPage} />
         </div>
-
-        {currentPage === 'main' && (
+        {currentPage === "main" && (
           <div className="container">
             <div className="journal-container">
               {/* Your journal pages go here */}
@@ -53,23 +51,29 @@ function App() {
               <div className="sidebar-foodlist">
                 <DatabasePage />
               </div>
-              <div className="iconbar">
-                <IconButton foodName="rice" icon={riceIcon} />
-                <IconButton foodName="banana" icon={bananaIcon} />
-              </div>
-              <div className="credits signature">
-                Built by Deon
-                <div class="seal">
-                  <FaBars className="hamburger-icon" />
+              <div className="sidebar-footer">
+                <div className="iconbar">
+                  <IconButton foodName="rice" icon={riceIcon} />
+                  <IconButton foodName="banana" icon={bananaIcon} />
+                </div>
+                <div className="credits signature">
+                  Built by Deon
+                  <div class="seal">
+                    <FaBars className="hamburger-icon" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         )}
-
-        {currentPage === 'database' && <DatabasePage />}
-        {currentPage === 'history' && <div><DailyDietList /></div>}
-        {currentPage === 'qa' && <QAPage />} {/* Render the QAPage when currentPage is 'qa' */}
+        {currentPage === "database" && <DatabasePage />}
+        {currentPage === "history" && (
+          <div>
+            <DailyDietList />
+          </div>
+        )}
+        {currentPage === "qa" && <QAPage />}{" "}
+        {/* Render the QAPage when currentPage is 'qa' */}
       </div>
     </DietProvider>
   );
